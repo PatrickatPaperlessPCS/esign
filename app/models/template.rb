8dfb69a66a049c7e84a54a1ea0df1d1d017ff223
PatrickatPaperlessPCS <patrick@paperlesspcs.com>
2016-01-11 17:25:28 -0600

    updated the template model and show view to try to improve image quality - unable to improve it much, probably result of original.  Will check and update

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,7 +1,7 @@
 class Template < ActiveRecord::Base
 
 
-  has_attached_file :attachment1, styles: { image: ["3000x3000>", :png] }, default_url: "/images/missing.png"
+  has_attached_file :attachment1, styles: { image: ["768x900", :png] }, default_url: "/images/missing.png",:convert_options => { density: "1050", quality: "100" }
   validates_attachment_content_type :attachment1, content_type: [/image/, "application/pdf"]
 
 belongs_to :user
