a43ec37a1e7bb3a90c8e15f963a25249211c3e29
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2016-01-28 21:20:56 +0300

    autofocus textfield, align close button

--- app/assets/javascripts/textfields.js
+++ app/assets/javascripts/textfields.js
@@ -15,29 +15,30 @@ jQuery(document).ready(function () {
     var textBoxBlurHandler = function(e) {
       document.createElementsOnClick = true;
     };
-    
+
     var div = jQuery('<div></div>').addClass('dynamic-textbox').css({
       position: 'absolute',
-      width: '150px',
       background: 'transparent',
-      top: cursorY + 'px',
-      left: cursorX + 'px',
+      top: cursorY - 10 + 'px',
+      left: cursorX - 20 + 'px',
     });
 
     var button = jQuery("<input type=button value='X' />").addClass('dynamic-textbox-button').
     on('click', function(e) {
       $(this).parent().remove();
+      e.stopPropagation();
     }).appendTo(div);
 
     var textbox = jQuery("<input type='text' name='document[dynamic_textfields_attributes][][text]' />").
       on('focus', textBoxFocusHandler).
       on('blur', textBoxBlurHandler).
       appendTo(div);
-    
+
     var positionX = jQuery("<input type=hidden name='document[dynamic_textfields_attributes][][left]' />").val(cursorX).appendTo(div);
     var positionY = jQuery("<input type=hidden name='document[dynamic_textfields_attributes][][top]' />").val(cursorY).appendTo(div);
-      
+
     div.appendTo(this);
+    textbox.focus();
   });
 });
 
