d8254e3f2fe3cb8299b7f8171845c6161953efc3
PatrickatPaperlessPCS <patrick@paperlesspcs.com>
2016-02-01 19:29:38 -0600

    apparently fixed sizing problem with min-width and min-height in bootstrap.css

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,7 +1,7 @@
 class Template < ActiveRecord::Base
 
 
-  has_attached_file :attachment1, styles: { image: ["1125x1500", :png] }, default_url: "/images/missing.png",:convert_options => { density: "1050", quality: "100" }
+  has_attached_file :attachment1, styles: { image: ["1125x1500", :png] }, default_url: "/images/missing.png",:convert_options => { invert: "true", density: "150", quality: "100" }
   validates_attachment_content_type :attachment1, content_type: [/image/, "application/pdf"]
 
 belongs_to :user
