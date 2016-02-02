8dd0ad2a00a73cfa3988d2b67d3c8ffe455850a6
PatrickatPaperlessPCS <patrick@paperlesspcs.com>
2016-02-01 19:46:51 -0600

    added are you sure text to templates/delete function, added limit of 10 templates

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,15 +1,15 @@
 class Template < ActiveRecord::Base
 
 
-  has_attached_file :attachment1, styles: { image: ["1125x1500", :png] }, default_url: "/images/missing.png",:convert_options => { invert: "true", density: "150", quality: "100" }
+  has_attached_file :attachment1, styles: { image: ["1125x1500", :png] }, default_url: "/images/missing.png",:convert_options => { density: "1050", quality: "100" }
   validates_attachment_content_type :attachment1, content_type: [/image/, "application/pdf"]
 
 belongs_to :user
 
-  validate :template_count_within_limit, :on => :new
+  validate :template_count_within_limit, :on => :create
 
   def template_count_within_limit
-    if self.user.templates(:reload).count >= 5
+    if self.user.templates(:reload).count >= 10
       errors.add(:base, "Exceeded template limit contact us at support@esignhealth.com to upgrade")
     end
   end
