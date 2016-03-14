bfe563a9599cf876a188cbcbcf1c11878b92a72e
PatrickatPaperlessPCS <patrick@paperlesspcs.com>
2016-03-14 16:53:44 -0500

    configured deploy.rb and production.rb for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,17 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'eSignHealth'
+set :repo_url, 'git@github.com:PatrickatPaperlessPCS/esign.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/opt/www/eSignHealth'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -32,7 +32,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
